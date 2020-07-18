00b93e9003c991e6eccee065cbe68b3ababfd4da
Gursaanj <gursaanjresides@gmail.com>
2020-07-18 00:21:34 -0700

    Initialize Project

---
+++ Dialogue Editror - Visual Scripting Tool/Assets/DialogueSystem/Editor/DialogueGraph.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueGraph : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
